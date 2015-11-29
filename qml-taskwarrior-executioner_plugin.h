92e747b44dcd907c88384c1cb8fea0470dba1106
Oscar Andreasson <oscar.andreasson@pelagicore.com>
2015-11-29 16:37:01 +0100

    Add MPL 2.0 copyright header to all sources

--- qml-taskwarrior-executioner_plugin.h
+++ qml-taskwarrior-executioner_plugin.h
@@ -1,3 +1,11 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Copyright (c) 2015 Oscar Andreasson
+ */
+
 #ifndef QML_TASKWARRIOR_EXECUTIONER_PLUGIN_H
 #define QML_TASKWARRIOR_EXECUTIONER_PLUGIN_H
 
